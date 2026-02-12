196a87f3b4af9c4e1f6c276819bf2debc5584285
Yonni-kug <ryonnimalon@gmail.com>
2026-02-12 11:37:10 -0500

    Update 5: Created a questions dart file, and quiz question setup

---
+++ lib/data/questions.dart
@@ -0,0 +1,51 @@
+import 'package:quiz_appsp26/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'How many notes in a scale?',
+    [
+      "Five notes",
+      "Six notes",
+      "Seven notes", //Correct answer
+      "Nine Notes"
+    ]
+  ),
+  QuizQuestion(
+    'What is the name of the musical symbol that indicates the pitch of a note?',
+    [
+      "Clef",
+      "Rest",
+      "Accidental",
+      "Time Signature"
+    ]
+  ),
+  QuizQuestion(
+    'If you wish to repeat a measure, what symbol would you use?',
+    [
+      "Repeat Sign",
+      "Fermata",
+      "Coda",
+      "Segno"
+    ]
+  ),
+
+    QuizQuestion(
+      "What is considered a Digital Audio Workshop?",
+      [
+        "A software used for recording, editing, and producing audio files", //Correct answer
+        "A physical workshop where musicians gather to create music",
+        "A type of musical instrument",
+        "A genre of music"
+      ]
+    ),
+
+    QuizQuestion(
+      "Pick the audio effects that are commonly used in music production.",
+      [
+        "Reverb",
+        "Delay",
+        "Distortion",
+        "All of the above" //Correct answer
+      ]
+    ),
+];
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen(this.startQuiz, {super.key});
-  final void Function() startQuiz;
+  final void Function() startQuiz;    
 @override
   Widget build(BuildContext context) {
     
