248f0ebe1c9412c4a123d087e2e1ebb795e661e4
Yonni-kug <ryonnimalon@gmail.com>
2026-03-05 12:19:36 -0500

    Update 12: Updated the results screen, added summary file to set up the structure of the results screen

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_appsp26/data/questions.dart';
+import 'package:quiz_appsp26/questions_summary.dart';
 class ResultsScreen extends StatelessWidget{
   const ResultsScreen({super.key,
    required this.chosenAnswers});
 
   final List<String> chosenAnswers;
+
+  List<Map<String, Object>> getSummaryData(){
+    List<Map<String, Object>> summary = [];
+    for (var i = 0; i < chosenAnswers.length; i++){
+      summary.add({
+        'question_index': i,
+        'question': questions[i].question,
+        'correct answer': questions[i].answers[0],
+        'user answer': chosenAnswers[i]
+
+      });
+    }
+    return summary;
+  }
   @override
   Widget build(context)
   {
+    final summaryData = getSummaryData();
+    final numTotalQuestions = getSummaryData().length;
+    final numTotalCorrect = summaryData.where((data) {
+      return data['user_answer'] == data['correct_answer'];
+    }).length;
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -14,8 +35,9 @@ class ResultsScreen extends StatelessWidget{
         child:  Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children:  [
-            const Text('You answered X out of Y questions Correctly', style: TextStyle(color: Colors.white)),
+            Text('You answered $numTotalCorrect out of $numTotalQuestions Correctly', style: const TextStyle(color: Colors.white)),
             const SizedBox(height:30,),
+            QuestionsSummary(summaryData: summaryData),
             const Text('List of Answers and Questions'),
             const SizedBox(height:30,),
             TextButton(
